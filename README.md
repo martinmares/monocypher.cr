396feb4a43adf43d8b5812a85b493fd7d6af2ab8
Martin Mareš <mares@datalite.cz>
2025-07-23 09:18:41 +0200

    Sync with fork, Upgrade monocypher.c to 4.0.1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 Crystal wrapper for a cryptographic library Monocypher ([Official site](https://monocypher.org), [Github page](https://github.com/LoupVaillant/Monocypher))
 
-Note: Sources of Monocypher (version 3.1.2 currently) are included in the shard and will be linked statically with application.
+Note: Sources of Monocypher (version 4.0.1 currently) are included in the shard and will be linked statically with application.
 On the other hand, if libmonocypher.so is present in library paths at the time of compilation, linker seems to prefer dynamic linking.
 
 Also included is the standard ED25519 cryptography which Monocypher supplies as an optional component.
@@ -53,7 +53,7 @@ bob_public = Crypto::PublicKey.new(secret: bob_secret)
 
 # sender part
 spawn do
-  alice_shared = Crypto::SymmetricKey.new(our_secret: alice_secret, their_public: bob_public)
+  alice_shared = Crypto::SymmetricKey.new(secret1: alice_secret, public2: bob_public)
   message = "This is a test message русский текст"
   plaintext = message.to_slice
   ciphertext = Bytes.new(plaintext.size + Crypto::OVERHEAD_SYMMETRIC)
@@ -62,7 +62,7 @@ spawn do
 end
 
 # receiver part
-bob_shared = Crypto::SymmetricKey.new(our_secret: bob_secret, their_public: alice_public)
+bob_shared = Crypto::SymmetricKey.new(secret2: bob_secret, public1: alice_public)
 ciphertext = channel.receive
 result = Bytes.new(ciphertext.size - Crypto::OVERHEAD_SYMMETRIC)
 Crypto.decrypt(key: bob_shared, input: ciphertext, output: result)
@@ -72,7 +72,7 @@ puts String.new(result)
 check `spec` dir for more usage examples (TODO - other examples)
 
 ## Hashes
-Monocypher includes functions for cryptographic hashes Blake2b and SHA-512, they are wrapped with simple crystal functions
+Monocypher includes functions for cryptographic hashes BLAKE2b and SHA-512, they are wrapped with simple crystal functions
 ```crystal
 Crypto.sha512("".to_slice).hexstring
 # "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
@@ -87,7 +87,31 @@ There is also `Digest` interface used in Crystal stdlib:
   digest << "123"
   digest.final.hexstring  # "3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2"
 
-  digest = Crypto::Digest::Blake2b.new
+  digest = Crypto::Digest::BLAKE2b.new(key: "".to_slice, hash_size: 64)
   digest << "123"
   digest.final.hexstring  # "ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923"
 ```
+
+## Development status
+Lowlevel wrapper currently covers all of Monocypher 4.0.1 release and can be called directly.
+
+Highlevel wrapper status:
+
+|Area | Status | Comment |
+|-----|--------|---------|
+| Constant Time Comparison | Not planned | Already in stdlib: see https://crystal-lang.org/api/1.7.2/Crypto/Subtle.html |
+| Memory Wipe | Done |  |
+| Authenticated Encryption | Partially done | TODO - streaming interface |
+| Blake2b Hash | Done |  |
+| SHA-512 hash | Partially done | TODO - hkdf mode |
+| Password Key Derivation | Partially done | TODO - expose additional options in high-level interface |
+| X25519 Key Exchange | Partially done | TODO - Generating pair |
+| Public Key Signatures | Partially done | TODO - XEdDSA implementation |
+| Ed25519 | Done |  |
+| ChaCha20 | - | Is high-level wrapper needed? |
+| Poly1305 | - | Is high-level wrapper needed? |
+| Elligator | - | Is high-level wrapper needed? |
+
+Other planned features:
+ - [ ] Inline docs for everything
+ - [ ] Input size checking everywhere
--- src/monocypher/hashes.cr
+++ src/monocypher/hashes.cr
@@ -2,23 +2,41 @@ require "./libmonocypher"
 require "digest/digest"
 
 module Crypto
+  # Calculate SHA-512 hash of `data`
   def self.sha512(data : Bytes) : Bytes
     Bytes.new(512 // 8).tap do |result|
       LibMonocypher.sha512(result, data, data.size)
     end
   end
 
+  # Calculate HMAC with SHA-512, that can be used for message authentication codes or as a random oracle.
+  def self.sha512_hmac(data : Bytes, key : Bytes) : Bytes
+    raise ArgumentError.new("key must not be empty") if key.size == 0
+    Bytes.new(512 // 8).tap do |result|
+      LibMonocypher.sha512_hmac(result, key, key.size, data, data.size)
+    end
+  end
+
+  # Calculate BLAKE2b hash of `data`, result will have `hash_size` bytes.
+  #
+  # `hash_size` - Length of hash, in bytes. Must be between 1 and 64. Anything below 16 is discouraged when using BLAKE2b as a message authentication code.
+  # Anything below 32 is discouraged when using BLAKE2b as a key derivation function (KDF).
+  #
+  # `key` - Some secret key. When uniformly random, one cannot predict the final hash without it.
   def self.blake2b(data : Bytes, *, hash_size : Int32 = 64, key : Bytes? = nil) : Bytes
     Bytes.new(hash_size).tap do |result|
       if key
-        LibMonocypher.blake2b_general(result, hash_size, key, key.size, data, data.size)
+        LibMonocypher.blake2b_keyed(result, hash_size, key, key.size, data, data.size)
       else
-        LibMonocypher.blake2b_general(result, hash_size, nil, 0, data, data.size)
+        LibMonocypher.blake2b(result, hash_size, data, data.size)
       end
     end
   end
 
   module Digest
+    # SHA-512, a cryptographically secure hash
+    #
+    # See [Crystal API docs](https://crystal-lang.org/api/latest/Digest.html) for details on how to use `Digest` interface
     class SHA512 < ::Digest
       @ctx = LibMonocypher::Sha512Ctx.new
 
@@ -47,10 +65,53 @@ module Crypto
       end
     end
 
-    class Blake2b < ::Digest
-      @ctx = LibMonocypher::Blake2bCtx.new
+    # Implement HMAC with SHA-512, and can be used for message authentication codes or as a random oracle.
+    #
+    # See [Crystal API docs](https://crystal-lang.org/api/latest/Digest.html) for details on how to use `Digest` interface
+    class SHA512HMAC < ::Digest
+      @ctx = LibMonocypher::Sha512HmacCtx.new
 
-      def initialize
+      def initialize(@key : Bytes)
+        raise ArgumentError.new("key must not be empty") if key.size == 0
+        reset_impl
+      end
+
+      # Hashes data incrementally.
+      def update_impl(data : Bytes) : Nil
+        LibMonocypher.sha512_hmac_update(pointerof(@ctx), data, data.size)
+      end
+
+      # Stores the output digest of #digest_size bytes in dst.
+      def final_impl(dst : Bytes) : Nil
+        LibMonocypher.sha512_hmac_final(pointerof(@ctx), dst)
+      end
+
+      # Resets the object to it's initial state.
+      def reset_impl : Nil
+        LibMonocypher.sha512_hmac_init(pointerof(@ctx), @key, @key.size)
+      end
+
+      # Returns the digest output size in bytes.
+      def digest_size : Int32
+        64
+      end
+    end
+
+    # BLAKE2b, a cryptographically secure hash based on the ideas of ChaCha20.
+    #
+    # See [Crystal API docs](https://crystal-lang.org/api/latest/Digest.html) for details on how to use `Digest` interface
+    class BLAKE2b < ::Digest
+      @ctx = LibMonocypher::BLAKE2bCtx.new
+
+      # Initializes Digest with given parameters
+      #
+      # `hash_size` - Length of hash, in bytes. Must be between 1 and 64. Anything below 32 is discouraged when using BLAKE2b as a general-purpose hash function.
+      # `key` - Some secret key. When uniformly random, one cannot predict the final hash without it.
+      def initialize(@hash_size = 64, @key : Bytes? = nil)
+        raise ArgumentError.new("hash_size must be between 1 and 64, received #{@hash_size}") unless (1..64).includes? @hash_size
+        if key = @key
+          raise ArgumentError.new("key.size must be between 0 and 64, received #{key.size}") unless (0..64).includes? key.size
+        end
         reset_impl
       end
 
@@ -66,12 +127,16 @@ module Crypto
 
       # Resets the object to it's initial state.
       def reset_impl : Nil
-        LibMonocypher.blake2b_init(pointerof(@ctx))
+        if key = @key
+          LibMonocypher.blake2b_keyed_init(pointerof(@ctx), @hash_size, key, key.size)
+        else
+          LibMonocypher.blake2b_init(pointerof(@ctx), @hash_size)
+        end
       end
 
       # Returns the digest output size in bytes.
       def digest_size : Int32
-        64
+        @hash_size
       end
     end
   end
